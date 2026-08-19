997d3292398215b9773134d6ac7b67eda5935b83
Angel <aguiberteaufranco@gmail.com>
2026-08-19 17:27:37 +0200

    ci: use preinstalled Chrome for browser tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   use: {
     baseURL: 'http://127.0.0.1:4173/UI---Forge/',
     ...devices['Desktop Chrome'],
+    channel: 'chrome',
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
